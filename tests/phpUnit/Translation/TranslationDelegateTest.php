d65e82dccec346d579e391de0d82b67f2112d311
danielzhang130 <danielzhang130@gmail.com>
2021-05-24 13:20:41 -0400

    - Add TranslationDelegate to handle multiple translation APIs and for error handling - More features like statistics and caching can be added to TranslationDelegate - Add translateComment API endpoint - Add relevant tests - Fix bug with php == and ===

---
+++ tests/phpUnit/Translation/TranslationDelegateTest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Translation;
+
+use App\Translation\TranslationApiInterface;
+use App\Translation\TranslationDelegate;
+use App\Translation\TranslationResult;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use Throwable;
+
+/**
+ * @internal
+ * @covers \App\Translation\TranslationDelegate
+ */
+class TranslationDelegateTest extends TestCase
+{
+  public function testSingleApi(): void
+  {
+    $api = $this->createMock(TranslationApiInterface::class);
+    $expected_result = new TranslationResult();
+    $api->expects($this->once())
+      ->method('translate')
+      ->willReturn($expected_result)
+    ;
+
+    $translation_delegate = new TranslationDelegate($api);
+
+    $actual_result = $translation_delegate->translate('test', 'en', 'fr');
+
+    $this->assertEquals($expected_result, $actual_result);
+  }
+
+  public function testTryNextApiIfFirstFails(): void
+  {
+    $api1 = $this->createMock(TranslationApiInterface::class);
+    $api2 = $this->createMock(TranslationApiInterface::class);
+
+    $api1->expects($this->once())
+      ->method('translate')
+      ->willReturn(null)
+    ;
+
+    $expected_result = new TranslationResult();
+    $api2->expects($this->once())
+      ->method('translate')
+      ->willReturn($expected_result)
+    ;
+
+    $translation_delegate = new TranslationDelegate($api1, $api2);
+
+    $actual_result = $translation_delegate->translate('test', 'en', 'fr');
+
+    $this->assertEquals($expected_result, $actual_result);
+  }
+
+  public function testAllApiFails(): void
+  {
+    $api1 = $this->createMock(TranslationApiInterface::class);
+    $api2 = $this->createMock(TranslationApiInterface::class);
+
+    $api1->expects($this->once())
+      ->method('translate')
+      ->willReturn(null)
+    ;
+
+    $api2->expects($this->once())
+      ->method('translate')
+      ->willReturn(null)
+    ;
+
+    $translation_delegate = new TranslationDelegate($api1, $api2);
+
+    $result = $translation_delegate->translate('test', 'en', 'fr');
+
+    $this->assertNull($result);
+  }
+
+  public function testInvalidLanguageCode(): void
+  {
+    $translation_delegate = new TranslationDelegate();
+    $invalid_code = [
+      '',
+      'x',
+      'xx',
+      'EN', // need to be lowercase en
+      'xxxxx',
+      'en-XX',
+      'EN-US', // need to be lowercase en
+      'en-us', // need to be uppercase US
+      'xx-US',
+    ];
+
+    foreach ($invalid_code as $code) {
+      try {
+        $translation_delegate->translate('test', $code, $code);
+        $this->fail('Should have thrown exception for language code '.$code);
+      } catch (Throwable $ex) {
+        $this->assertInstanceOf(InvalidArgumentException::class, $ex);
+      }
+    }
+  }
+}
