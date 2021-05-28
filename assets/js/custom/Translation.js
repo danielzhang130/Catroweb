cba17e2c23026078fb82dcabd9bdba24d314e146
joshschaefer <je2schae@uwaterloo.ca>
2021-05-28 15:09:03 -0400

    Added basic version of translation feature within comment section

--- assets/js/custom/Translation.js
+++ assets/js/custom/Translation.js
@@ -2,6 +2,29 @@
 
 // eslint-disable-next-line no-unused-vars
 function Translation (hasDescription, hasCredit) {
+
+  $(document).on('click', '.comment-translation-button', function () {
+    const commentId = $(this).attr('id').substring('comment-translation-button-'.length)
+
+    const textElement = $('#comment-text-' + commentId)
+    const translationTextElement = $('#comment-text-translation-' + commentId)
+    const translationCreditElement = $('#comment-translation-credit-' + commentId)
+    translateContent(textElement, translationTextElement, translationCreditElement)
+
+    $(this).hide()
+    $('#remove-comment-translation-button-' + commentId).show()
+    $('#comment-text-wrapper-' + commentId).slideUp()
+    $('#comment-translation-wrapper-' + commentId).slideDown()
+  })
+
+  $(document).on('click', '.remove-comment-translation-button', function () {
+    const commentId = $(this).attr('id').substring('remove-comment-translation-button-'.length)
+    $(this).hide()
+    $('#comment-translation-button-' + commentId).show()
+    $('#comment-translation-wrapper-' + commentId).slideUp()
+    $('#comment-text-wrapper-' + commentId).slideDown()
+  })
+
   const elementsToTranslate = [document.getElementById('name')]
 
   if (hasDescription) {
@@ -17,10 +40,23 @@ function Translation (hasDescription, hasCredit) {
     document.documentElement.lang
   )
 
-  $('.comment-translate-button').each(function () {
-    const commentId = $(this).attr('id').substring('comment-translate-button-'.length)
-    translation(this, document.getElementById('comment-text-' + commentId), document.documentElement.lang)
-  })
+  function translateContent(textElement, translationTextElement, translationCreditElement) {
+    // var list = '{{ countries|json_encode|raw }}';
+    content = textElement.text()
+    translationCreditElement.text("Translated by iTranslate from English")
+    translationTextElement.text(content + " translation")
+    // $.ajax({
+    //   url: '../api/comment/1/translate',
+    //   type: 'get',
+    //   data: { text: "test", target_language="fr" },
+    //   success: function (data) {
+    //     console.log("test success")
+    //   },
+    //   error: function () {
+    //     console.log("test error")
+    //   }
+    // })
+  }
 
   function translation (buttonElement, textElements, targetLang) {
     let text = ''
