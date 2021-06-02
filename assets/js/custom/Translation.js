7eb92d7056038b5236cfcc0de60703c0b7698228
joshschaefer <je2schae@uwaterloo.ca>
2021-06-02 14:38:13 -0400

    Updated translation feature to use API and created tests

--- assets/js/custom/Translation.js
+++ assets/js/custom/Translation.js
@@ -2,19 +2,36 @@
 
 // eslint-disable-next-line no-unused-vars
 function Translation (hasDescription, hasCredit) {
+  var languageMap = {}
+  var targetLanguage = document.documentElement.lang
+
+  $(document).ready(function () {
+    $.ajax({
+      url: '../languages',
+      type: 'get',
+      success: function (data) {
+        languageMap = data
+      }
+    })
+  })
+
+  $('.mdc-list-item__text').each(function() {
+    if ($(this).parent()[0].hasAttribute('selected')) {
+      targetLanguage = fixLanguageCode($(this).parent().attr('data-value'))
+    }
+  })
 
   $(document).on('click', '.comment-translation-button', function () {
     const commentId = $(this).attr('id').substring('comment-translation-button-'.length)
 
-    const textElement = $('#comment-text-' + commentId)
-    const translationTextElement = $('#comment-text-translation-' + commentId)
-    const translationCreditElement = $('#comment-translation-credit-' + commentId)
-    translateContent(textElement, translationTextElement, translationCreditElement)
-
     $(this).hide()
-    $('#remove-comment-translation-button-' + commentId).show()
-    $('#comment-text-wrapper-' + commentId).slideUp()
-    $('#comment-translation-wrapper-' + commentId).slideDown()
+
+    if ($('#comment-text-translation-' + commentId).attr('lang') != targetLanguage) {
+      $('#comment-translation-loading-spinner-' + commentId).show()
+      translateComment(commentId)
+    } else {
+      openTranslatedComment(commentId)
+    }
   })
 
   $(document).on('click', '.remove-comment-translation-button', function () {
@@ -24,6 +41,71 @@ function Translation (hasDescription, hasCredit) {
     $('#comment-translation-wrapper-' + commentId).slideUp()
     $('#comment-text-wrapper-' + commentId).slideDown()
   })
+
+  function fixLanguageCode(languageCode) {
+    if (languageCode.length > 2) {
+      languageCode = languageCode.split('')
+      languageCode[2] = '-'
+      languageCode = languageCode.join('')
+    }
+
+    return languageCode
+  }
+
+  function getTranslationProvider(provider) {
+    if (provider == "itranslate") {
+      return "iTranslate "
+    }
+    return ""
+  }
+
+  function openGoogleTranslatePage(commentId) {
+    var text = document.getElementById('comment-text-' + commentId).innerText
+    window.open(
+      'https://translate.google.com/?q=' + encodeURIComponent(text) + '&sl=auto&tl=' + targetLanguage,
+      '_self'
+    );
+  }
+
+  function setTranslatedCommentData(commentId, data) {
+    $('#comment-text-translation-' + commentId).text(data.translation)
+    $('#comment-text-translation-' + commentId).attr('lang', data.target_language)
+
+    var provider = $('#comment-translation-provider-' + commentId)
+      .text()
+      .replace('%provider%', getTranslationProvider(data.provider))
+    $('#comment-translation-provider-' + commentId).text(provider)
+    $('#comment-translation-source-language-' + commentId).text(languageMap[data.source_language])
+    $('#comment-translation-target-language-' + commentId).text(languageMap[data.target_language])
+  }
+
+  function openTranslatedComment(commentId) {
+    $('#comment-translation-loading-spinner-' + commentId).hide()
+    $('#remove-comment-translation-button-' + commentId).show()
+    $('#comment-translation-wrapper-' + commentId).slideDown()
+    $('#comment-text-wrapper-' + commentId).slideUp()
+  }
+
+  function commentNotTranslated(commentId) {
+    $('#comment-translation-loading-spinner-' + commentId).hide()
+    $('#comment-translation-button-' + commentId).show()
+    openGoogleTranslatePage(commentId)
+  }
+
+  function translateComment(commentId) {
+    $.ajax({
+      url: '../translate/comment/' + commentId,
+      type: 'get',
+      data: { target_language: targetLanguage },
+      success: function (data) {
+        setTranslatedCommentData(commentId, data)
+        openTranslatedComment(commentId)
+      },
+      error: function () {
+        commentNotTranslated(commentId)
+      }
+    })
+  }
 
   const elementsToTranslate = [document.getElementById('name')]
 
@@ -39,24 +121,6 @@ function Translation (hasDescription, hasCredit) {
     elementsToTranslate,
     document.documentElement.lang
   )
-
-  function translateContent(textElement, translationTextElement, translationCreditElement) {
-    // var list = '{{ countries|json_encode|raw }}';
-    content = textElement.text()
-    translationCreditElement.text("Translated by iTranslate from English")
-    translationTextElement.text(content + " translation")
-    // $.ajax({
-    //   url: '../api/comment/1/translate',
-    //   type: 'get',
-    //   data: { text: "test", target_language="fr" },
-    //   success: function (data) {
-    //     console.log("test success")
-    //   },
-    //   error: function () {
-    //     console.log("test error")
-    //   }
-    // })
-  }
 
   function translation (buttonElement, textElements, targetLang) {
     let text = ''
--- tests/behat/features/web/translation/translate_comments.feature
+++ tests/behat/features/web/translation/translate_comments.feature
@@ -16,7 +16,28 @@ Feature: Project title, description and credits should be translatable via a but
   Scenario: Translate button should translate the corresponding comment
     Given I am on "/app/project/1"
     And I wait for the page to be loaded
-    Then the element "#comment-translate-button-1" should exist
-    Then the element "#comment-translate-button-2" should exist
-    And the element "#comment-translate-button-1" should have a attribute "href" with value "https://translate.google.com/?q=c1"
-    And the element "#comment-translate-button-2" should have a attribute "href" with value "https://translate.google.com/?q=c2"
+    Then the element "#comment-translation-button-1" should exist
+    When I click "#comment-translation-button-1"
+    Then the element "#comment-translation-loading-spinner-1" should be visible
+    And I wait for AJAX to finish
+    Then the element "#remove-comment-translation-button-1" should be visible
+    And the element "#comment-translation-wrapper-1" should be visible
+    When I click "#remove-comment-translation-button-1"
+    Then the element "#comment-translation-button-1" should be visible
+    And the element "#comment-text-wrapper-1" should be visible
+  
+  Scenario: Comment should only be translated by API once
+    Given I am on "/app/project/1"
+    And I wait for the page to be loaded
+    Then the element "#comment-translation-button-2" should exist
+    When I click "#comment-translation-button-2"
+    Then the element "#comment-translation-loading-spinner-2" should be visible
+    And I wait for AJAX to finish
+    Then the element "#remove-comment-translation-button-2" should be visible
+    And the element "#comment-translation-wrapper-2" should be visible
+    When I click "#remove-comment-translation-button-2"
+    Then the element "#comment-translation-button-2" should be visible
+    And the element "#comment-text-wrapper-2" should be visible
+    When I click "#comment-translation-button-2"
+    Then the element "#remove-comment-translation-button-2" should be visible
+    And the element "#comment-translation-wrapper-2" should be visible
